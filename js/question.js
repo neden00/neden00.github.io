1f213a03b98c4301a2e225123c55924f7f58cc8f
neden00 <alimelik55@gmail.com>
2020-04-30 21:47:18 +0300

    second

---
+++ js/question.js
@@ -0,0 +1,89 @@
+let qdb = [{
+        "question": "İsteyen herkes iman edebilir.",
+        "correct": false,
+        "answer": `
+        <p>Muhammed Mekke'deyken, Mekkelileri ikna edemediği için aşağıdaki ayet inmiştir. Bu ayet dışında da birkaç 
+        kez Allah'ın kalpleri mühürlemesinden bahsedilmektedir.</p>
+
+        <blockquote style="font-size: 0.9em">
+            Onlardan seni Kur’an okurken dinleyenler de vardır. Fakat <b>onu anlamalarına engel olmak için kalplerinin
+            üstüne örtüler çektik, kulaklarına da ağırlık verdik</b>. Onlar her türlü mûcizeyi görseler bile yine de ona inanmazlar. 
+            Hatta o kâfirler sana geldiklerinde, "Bu Kur’an eskilerin masallarından başka bir şey değildir" diyerek seninle tartışırlar. (En'am/25)
+        </blockquote>
+
+        <p>Daha sonra Mekke'nin fethiyle her nasılsa bahsedilen müşrikler iman etmiştir.</p>`
+    },
+    {
+        "question": "Muhammed kendi geliniyle evlenmiştir.",
+        "correct": true,
+        "answer": `
+        <p>Günlerden birgün Allah resulü, evlatlığı Zeyd'in evine gidiyor ancak Zeyd o sırada evde değil. 
+        Eşi Zeynep'i uygunsuz bir kıyafetle görüyor. Tabii aşık oluyor Peygamberimiz evlatlığının eşine. 
+        Gel zaman git zaman, yüreğindeki sevdayı bastıramayıp gidip Zeynep'i eşi Zeyd'den istiyor. 
+        Zeyd ise çok zorlanarak da olsa eşini boşuyor ve Muhammed'le evlendiriyor (Kaynak: Kütüb-i Sitte).<br>
+        Bu kararında alttaki ayet de oldukça etkili oluyor.</p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Bir mümin erkek veya bir mümin kadının, Allah ve resulü bir emir ve hüküm verdiklerinde artık işlerinde bundan başkasını seçme hakları olamaz. 
+        Allah’ın ve resulünün emrine itaat etmeyenler doğru yoldan açıkça sapmışlardır. (Ahzab/36)
+        </blockquote>
+
+        <p>
+        Çıkan dedikodular karşısında Muhammed'in evlatlıklarınızın eşleriyle evlenebileceğinizi göstermek için Zeyd'in eşi Zeynep ile evlendirildiğini 
+        söyleyen aşağıdaki ayet iniyor. 
+        </p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Hani Allah'ın nimet verdiği, senin de kendisine iyilik ettiğin kimseye: Eşini yanında tut, Allah'tan kork! diyordun. 
+        Allah'ın açığa vuracağı şeyi, insanlardan çekinerek içinde gizliyordun. Oysa asıl korkmana layık olan Allah'tır. 
+        Zeyd, o kadından ilişiğini kesince biz onu sana nikahladık ki evlatlıkları, karılarıyla ilişkilerini kestiklerinde 
+        (o kadınlarla evlenmek isterlerse) müminlere bir güçlük olmasın. Allah'ın emri yerine getirilmiştir. (Ahzab/37)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an'da zina yasaklanmıştır.",
+        "correct": true,
+        "answer": `<p>Kur'an'da zina İsra/32 ile yasaklanmış, Nur/2 ile cezası acımadan 100 sopa vurmak olarak belirlenmiştir. Peki aslında zina ne derece engellenmiştir?</p>
+        <p>1. Zinanın ispatı için dört tane şahit olması gerekmektedir (Nisa/15). Zira Muhammed'in eşi Ayşe'de zina ile suçlanmış ama bu ayet dolayısıyla affedilmiştir. 
+        Peki sizce zina yapan birinin dört adet şahitle yakalanma ihtimali nedir?</p>
+        <p>2. Dört adet eş (Nisa/3), sınırsız cariye (Nisa/3, Nisa/24) ve ücreti ödenme şartıyla geçici nikâh izni (Nisa/24) varken zinadan uzak durun demenin mantığı nedir?</p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Sakın zinaya yaklaşmayın; doğrusu bu çirkindir, kötü bir yoldur. (İsra/32)
+        </blockquote>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Zina eden kadın ve erkeğin her birine yüzer değnek vurun. Allah'a ve ahiret gününe inanıyorsanız, Allah'ın dini konusunda o ikisine acımayın. 
+        Onların ceza görmesine, inananlardan bir topluluk da şahit olsun. (Nur/2)
+        </blockquote>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Eğer, (velisi olduğunuz) yetim kızlar (ile evlenip onlar) hakkında adaletsizlik etmekten korkarsanız, (onları değil), size helâl olan (başka) 
+        kadınlardan ikişer, üçer, dörder olmak üzere nikâhlayın.[105] Eğer (o kadınlar arasında da) adaletli davranmayacağınızdan korkarsanız, 
+        o taktirde bir tane alın veya sahip olduğunuz (cariyeler) ile yetinin. Bu, adaletten ayrılmamanız için daha uygundur. (Nisa/3)
+        </blockquote>
+        
+        <blockquote style=\"font-size: 0.9em\">
+        Evli kadınlarla evlenmeniz de haram kılındı. Maliki bulunduğunuz cariyeler müstesna, bunlar, Allah'ın üzerinize farz kıldığı hükümlerdir. 
+        <b>Bunlardan başkasını, zinadan kaçınıp, iffetli olarak, mallarınızla istemeniz size helal kılındı. Onlardan faydalandığınıza mukabil, 
+        kararlaştırılmış olan mehirlerini verin</b>; kararlaştırılandan başka, karşılıklı hoşnud olduğunuz hususda size bir sorumluluk yoktur. 
+        Allah Bilen'dir, Hakim'dir. (Nisa/24) mehir: ücret
+        </blockquote>
+        `
+    },
+    {
+        "question": "Kur'an'da evrenin genişlediği yazmaktadır.",
+        "correct": false,
+        "answer": "<p>BURAYA AÇIKLAMA GELECEK.</p><blockquote style=\"font-size: large !important;\">Burada ayet yer alacak.</blockquote><p>vesaire.</p>"
+    },
+    {
+        "question": "Kur'an'ın tamamı Allah'ın sözü müdür?.",
+        "correct": false,
+        "answer": "<p>BURAYA AÇIKLAMA GELECEK.</p><blockquote style=\"font-size: large !important;\">Burada ayet yer alacak.</blockquote><p>vesaire.</p>"
+    },
+    {
+        "question": "Kur'an'da Allah yanılmıştır.",
+        "correct": true,
+        "answer": "<p>BURAYA AÇIKLAMA GELECEK.</p><blockquote style=\"font-size: large !important;\">Burada ayet yer alacak.</blockquote><p>vesaire.</p>"
+    }
+]
