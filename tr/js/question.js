641589080937eb29328dbc03e020384fef658f66
neden00 <alimelik55@gmail.com>
2020-05-02 23:53:46 +0300

    Fixes

--- tr/js/question.js
+++ tr/js/question.js
@@ -106,7 +106,7 @@ let qdb = [{
         [...]Erkeklerinizden iki şahid tutun; eğer iki erkek bulunmazsa, şahidlerden razı olacağınız bir erkek, biri unuttuğunda diğeri ona hatırlatacak iki kadın olabilir. [...] (Bakara/282)
         </blockquote>
 
-        <p>Kadınlar yalancı mıdır? Unutkan mıdır? Bu ayetin inme sebebi nedir? <br> Alttaki hadis de hem kadını hem Yahudileri adeta aşağılar.</p>
+        <p>Kadınlar yalancı mıdır? Unutkan mıdır? Bu ayetin inme sebebi nedir? <br> Alttaki hadis de hem kadını hem Yahudileri kötü görmenin bir örneğidir.</p>
 
         <blockquote style="font-size: 0.9em">
         Resulullah as buyurdular ki: “Biriniz sütresiz olarak namaz kılarsa (önün­den geçtiği takdirde) şunlar namazı bozar: eşek, domuz, yahudi, mecûsi, kadın. Namazın bozulmaması için onun önünden, bunların bir taş atımlık uzaktan geçmesi kifâyet eder.” <br> (Buhari, Salat 90, İlm 18, Ezan 161, Cezau's-Sayd 25; Müslim, Salat 254, (504); Muvatta, Kasru's-Sala)
@@ -116,8 +116,7 @@ let qdb = [{
         "question": "Muhammed kendi geliniyle evlenmiştir.",
         "correct": true,
         "answer": `
-        <p>Günlerden birgün Allah resulü, evlatlığı Zeyd'in evine gidiyor ancak Zeyd o sırada evde değil. 
-        Zeyd'in eşi (aynı zamanda Muhammed'in halasının kızı olan) Zeynep'i uygunsuz bir kıyafetle görüyor. Tabii aşık oluyor Peygamberimiz evlatlığının eşine.Gel zaman git zaman, yüreğindeki sevdayı bastıramayıp gidip Zeynep'i eşi Zeyd'den istiyor. Zeyd ise çok zorlanarak da olsa eşini boşuyor ve Muhammed'le evlendiriyor (Kaynak: Kütüb-i Sitte).<br>Bu kararında alttaki ayet de oldukça etkili oluyor.</p>
+        <p>Günlerden birgün Allah resulü, evlatlığı Zeyd'in evine gider ancak Zeyd o sırada evde değildir. Muhammed, Zeyd'in eşi (aynı zamanda Muhammed'in halasının kızı) Zeynep'i uygunsuz bir kıyafetle görür. Tabii aşık olur Peygamberimiz evlatlığının eşine.Gel zaman git zaman, yüreğindeki sevdayı bastıramaz gidip Zeynep'i eşi Zeyd'den ister. Zeyd ise çok zorlanarak da olsa eşini boşar ve Muhammed'le evlendiririr (Kaynak: Kütüb-i Sitte).<br>Bu kararında alttaki ayet de oldukça etkili olmuştur.</p>
 
         <blockquote style=\"font-size: 0.9em\">
         Bir mümin erkek veya bir mümin kadının, Allah ve resulü bir emir ve hüküm verdiklerinde artık işlerinde bundan başkasını seçme hakları olamaz. Allah’ın ve resulünün emrine itaat etmeyenler doğru yoldan açıkça sapmışlardır. (Ahzab/36)
@@ -141,12 +140,6 @@ let qdb = [{
         "answer": `<p>Kur'an'da zina İsra/32 ile yasaklanmış, Nur/2 ile cezası acımadan 100 sopa vurmak olarak belirlenmiştir. 
         Peki aslında zina ne derece engellenmiştir?</p>
 
-        <p>1. Zinanın ispatı için dört tane şahit olması gerekmektedir (Nisa/15). Zira Muhammed'in eşi Ayşe'de zina ile suçlanmış ama 
-        bu ayet dolayısıyla affedilmiştir. Peki sizce zina yapan birinin dört adet şahitle yakalanma ihtimali nedir?</p>
-
-        <p>2. Dört adet eş (Nisa/3), sınırsız cariye (Nisa/3, Nisa/24) ve ücreti ödenme şartıyla geçici nikâh izni (Nisa/24) varken 
-        zinadan uzak durun demenin mantığı nedir?</p>
-
         <blockquote style=\"font-size: 0.9em\">
         Sakın zinaya yaklaşmayın; doğrusu bu çirkindir, kötü bir yoldur. (İsra/32)
         </blockquote>
@@ -155,6 +148,12 @@ let qdb = [{
         Zina eden kadın ve erkeğin her birine yüzer değnek vurun. Allah'a ve ahiret gününe inanıyorsanız, Allah'ın dini konusunda o ikisine acımayın. 
         Onların ceza görmesine, inananlardan bir topluluk da şahit olsun. (Nur/2)
         </blockquote>
+
+        <p>1. Zinanın ispatı için dört tane şahit olması gerekmektedir (Nisa/15). Zira Muhammed'in eşi Ayşe'de zina ile suçlanmış ama 
+        bu ayet dolayısıyla affedilmiştir. Peki sizce zina yapan birinin dört adet şahitle yakalanma ihtimali nedir?</p>
+
+        <p>2. Dört adet eş (Nisa/3), sınırsız cariye (Nisa/3, Nisa/24) ve ücreti ödenme şartıyla geçici nikâh izni (Nisa/24) varken 
+        zinadan uzak durun demenin mantığı nedir?</p>
 
         <blockquote style=\"font-size: 0.9em\">
         Eğer, (velisi olduğunuz) yetim kızlar (ile evlenip onlar) hakkında adaletsizlik etmekten korkarsanız, (onları değil), size helâl olan (başka) 
@@ -172,7 +171,7 @@ let qdb = [{
     },
     {
         "question": "Muhammed ayı ikiye yarmıştır.",
-        "correct": true,
+        "correct": false,
         "answer": `
         <p>O dönemdeki başka hiçbir kaynakta yer almayan, hiçbir dayanağı olmayan bu olay (şakk-ı kamer) Kur'an'da yer almaktadır.</p>
 
@@ -182,25 +181,23 @@ let qdb = [{
     },
     {
         "question": "Kur'an'da evrenin genişlediği yazmaktadır.",
-        "correct": true,
+        "correct": false,
         "answer": `
-        <p>İslamcılar aşağıdaki ilk ayet ile, modern astronomiyle keşfedilen evrenin genişlediği bilgisinin Kur'an'da mevcut olduğunu iddia eder 
-        ve bunu bir mucize sayar.</p>
+        <p>İslamcılar aşağıdaki ilk ayet ile, modern astronomiyle keşfedilen evrenin genişlediği bilgisinin Kur'an'da mevcut olduğunu iddia eder ve bunu bir mucize sayar.</p>
 
         <blockquote style="font-size: 0.9em">
         Göğü, gücümüzle Biz kurduk; şüphesiz biz onu genişleticiyiz. (Zariyat/47 Eski Diyanet Meali)
         </blockquote>
 
         <blockquote style="font-size: 0.9em">
-        Göğü kudretimizle biz kurduk ve şüphesiz bizim (her şeye) gücümüz yeter. (Zariyat/47 Yeni Diyanet Meali)
-        </blockquote>
-
-        <blockquote style="font-size: 0.9em">
         Yeri de biz döşedik. Biz ne güzel döşeyiciyiz.! (Zariyat/48)
         </blockquote>
 
-        <p>Peki ilk ayet bir mucize ise, ikinci ayette de yeryüzünün genişlediğini söylemiyor mu? Bu argüman karşısında ise diyanet ayetin mealini 
-        değiştirme yoluna gitmiştir.</p>`
+        <p>Peki ilk ayet bir mucize ise, ikinci ayette de yeryüzünün genişlediğini söylemiyor mu? Bu argüman karşısında ise diyanet ayetin mealini değiştirme yoluna gitmiştir.</p>
+        
+        <blockquote style="font-size: 0.9em">
+        Göğü kudretimizle biz kurduk ve şüphesiz bizim (her şeye) gücümüz yeter. (Zariyat/47 Yeni Diyanet Meali)
+        </blockquote>`
     },
     {
         "question": "Kur'an'da Allah yanılmıştır.",
@@ -234,7 +231,7 @@ let qdb = [{
     {
         "question": "Kur'an köleliği yasaklar.",
         "correct": false,
-        "answer": `<p></p>
+        "answer": `<p>Eklenecek.</p>
 
         <blockquote style="font-size: 0.9em">
         </blockquote>`
@@ -251,8 +248,7 @@ let qdb = [{
     {
         "question": "Kur'an düşünme organı olan beyinden bahseder.",
         "correct": false,
-        "answer": `<p>Kur'an, beyinden hiç bahsetmez; düşünme, inanma, sorgulama, şüphelenme yetilerinin hepsinin kalp ile yapıldığını söyler. 
-        Böyle bir şey olmadığını günümüzde pek iyi biliyoruz.</p>
+        "answer": `<p>Kur'an, beyinden hiç bahsetmez; düşünme, inanma, sorgulama, şüphelenme yetilerinin hepsinin kalp ile yapıldığını söyler. Böyle bir şey olmadığını günümüzde pek iyi biliyoruz.</p>
 
         <blockquote style="font-size: 0.9em">
         O (insan), atılan bir sudan yaratıldı. O su, bel (omurga) ve göğüs kafesi arasından çıkar. (Tarık/6-7)
@@ -261,26 +257,23 @@ let qdb = [{
     {
         "question": "Cuma namazı erkeklere farzdır.",
         "correct": false,
-        "answer": `<p>Çoğu kesim tarafından öyle kabul edilse de Kur'an'da cuma namazıyla ilgili yek ifade alttakidir.</p>
+        "answer": `<p>Çoğu kesim tarafından öyle kabul edilse de Kur'an'da cuma namazıyla ilgili tek ifade alttakidir.</p>
 
         <blockquote style="font-size: 0.9em">
-        Ey iman edenler! Cuma günü namaz için çağrı yapıldığı zaman, hemen Allah’ın zikrine koşun ve alışverişi bırakın. 
-        Eğer bilirseniz bu, sizin için daha hayırlıdır. (Cuma/9)
+        Ey iman edenler! Cuma günü namaz için çağrı yapıldığı zaman, hemen Allah’ın zikrine koşun ve alışverişi bırakın. Eğer bilirseniz bu, sizin için daha hayırlıdır. (Cuma/9)
         </blockquote>`
     },
     {
         "question": "Kur'an evrenseldir.",
         "correct": false,
-        "answer": `<p>Alttaki ayetler çoğu İslamcı tarafından Mekke ve çevresi tüm dünyayı kastetmiştir olarak açıklanmaktadır. 
-        Peki Allah bunun yanlış anlaşılabileceğini düşünemedi de mi bu ayeti indirdi?</p>
+        "answer": `<p>Alttaki ayetler çoğu İslamcı tarafından Mekke ve çevresi tüm dünyayı kastetmiştir olarak açıklanmaktadır. Peki Allah bunun yanlış anlaşılabileceğini düşünemedi de mi bu ayeti indirdi?</p>
 
         <blockquote style="font-size: 0.9em">
-        Bu indirdiğimiz, kendinden öncekileri doğrulayan, Mekkelileri ve etrafındakileri uyaran mübarek Kitap'dır. Ahirete inananlar buna inanırlar, 
-        namazlarına da devam ederler. (Enam/92) Eski Diyanet Meali
+        Bu indirdiğimiz, kendinden öncekileri doğrulayan, Mekkelileri ve etrafındakileri uyaran mübarek Kitap'dır. Ahirete inananlar buna inanırlar, namazlarına da devam ederler. (En'am/92) Eski Diyanet Meali
         </blockquote>
+
         <blockquote style="font-size: 0.9em">
-        İşte bu (Kur’an) da, bereket kaynağı, kendinden öncekileri (ilâhî kitapları) tasdik eden ve şehirler anasını (Mekke’yi) ve bütün çevresini 
-        (tüm insanlığı) uyarasın diye indirdiğimiz bir kitaptır. Ahirete iman edenler, ona da inanırlar. Onlar namazlarını vaktinde kılarlar. (Enam/92) Yeni Diyanet Meali
+        İşte bu (Kur’an) da, bereket kaynağı, kendinden öncekileri (ilâhî kitapları) tasdik eden ve şehirler anasını (Mekke’yi) ve bütün çevresini (tüm insanlığı) uyarasın diye indirdiğimiz bir kitaptır. Ahirete iman edenler, ona da inanırlar. Onlar namazlarını vaktinde kılarlar. (En'am/92) Yeni Diyanet Meali
         </blockquote>
         
         <p>Bu ayetde Kur'an'ın anlaşılabilmesi için Arapça indiğini söyler. Peki Arapça indiğinde anlaşılabileceği kişiler Araplar ile sınırlı değil midir?</p>
