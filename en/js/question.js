e739d8c1842fb55bd76ebd8722eb9105f3962086
neden00 <alimelik55@gmail.com>
2020-05-02 19:23:09 +0300

    English

---
+++ en/js/question.js
@@ -0,0 +1,294 @@
+let qdb = [{
+        "question": "İsteyen herkes iman edebilir.",
+        "correct": false,
+        "answer": `
+        <p>Muhammed Mekke'deyken, Mekkelileri ikna edemediği için aşağıdaki ayet inmiştir. Bahsedilen müşrikler daha sonra Mekke'nin fethiyle 
+        her nasılsa iman edenlere katılmıştır.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Onlardan seni Kur’an okurken dinleyenler de vardır. Fakat <b>onu anlamalarına engel olmak için kalplerinin üstüne örtüler çektik, kulaklarına da ağırlık verdik</b>. Onlar her türlü mûcizeyi görseler bile yine de ona inanmazlar. Hatta o kâfirler sana geldiklerinde, "Bu Kur’an eskilerin masallarından başka bir şey değildir" diyerek seninle tartışırlar. (En'am/25)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Sen onların doğru yola erişmelerine aşırı istek göstersen de şüphesiz Allah saptırdığı kimseyi doğru yola iletmez. Onların yardımcıları da yoktur. (Nahl/37)
+        </blockquote>
+        
+        <blockquote style="font-size: 0.9em">
+        Allah kimi doğru yola iletmek isterse onun kalbini İslam'a açar; kimi de saptırmak isterse göğe çıkıyormuş gibi kalbini iyice daraltır. Allah inanmayanların üstüne işte böyle murdarlık verir. (En'am/125)
+        </blockquote>
+
+        `
+    },
+    {
+        "question": "Kur'an'ın tamamı Allah'ın sözü müdür?.",
+        "correct": false,
+        "answer": `
+        <p>Sizce Allah'ın aşağıdaki gibi cümleler kurması mümkün müdür? Diyanet kimi yerde bu ayetleri parantezlerle kurtarmaya çalışmıştır ancak bizim incelememiz gereken Kur'an'ın kendisidir.</p>
+
+        <blockquote style="font-size: 0.9em">
+        (De ki:) Şüphesiz ben size O’nun tarafından gönderilmiş bir uyarıcı ve müjdeleyiciyim. (Hud/2)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Hakkında ayrılığa düştüğünüz herhangi bir şeyin hükmü Allah’a aittir. İşte bu, Rabbim Allah’tır. Yalnız O’na tevekkül ettim ve ancak O’na yöneliyorum. (Şura/10)
+        </blockquote>
+        
+        <blockquote style="font-size: 0.9em">
+        Allah ile beraber başka bir ilâh edinmeyin. Gerçekten ben, size, Allah tarafından gönderilmiş açık bir uyarıcıyım. (Zariyat/51)
+        </blockquote>`
+    },
+    {
+        "question": "İslam şüphe edip, sorgulamaya açıktır.",
+        "correct": false,
+        "answer": `<p></p>
+
+        <blockquote style="font-size: 0.9em">
+        Hak (ancak) Rabbindendir. Artık, <b>sakın şüpheye düşenlerden olma!</b> (Bakara/147)
+        </blockquote>
+        
+        <blockquote style="font-size: 0.9em">
+        (De ki): Allah'dan başka bir hakem mi arayacağım? Halbuki size Kitab'ı açık olarak indiren O'dur. Kendilerine kitap verdiğimiz kimseler, Kur'an'ın gerçekten Rabbin tarafından indirilmiş olduğunu bilirler. Sakın şüpheye düşenlerden olma! (En'am/114)
+        <blockquote style="font-size: 0.9em">`
+        
+    },
+    {
+        "question": "İslam hoşgörü dinidir.",
+        "correct": false,
+        "answer": `
+                
+        <blockquote style="font-size: 0.9em">
+        Dinini değiştireni öldürün. (Buhari, Cihad, 149)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Ey inananlar! Yahudi ve hıristiyanları dost edinmeyin. Onlar birbirlerinin dostlarıdırlar. Sizden kim onları dost edinirse, kuşkusuz o da onlardandır. Şüphesiz Allah, zalimler topluluğunu doğruya iletmez. (Maide/51)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Haram aylar çıkınca, müşrikleri bulduğunuz yerde öldürün, esir alın, kuşatın ve onları her geçit yerinde gözetleyin. Şayet tövbe ederler, namazlarını kılarlar ve zekâtlarını verirlerse artık onları serbest bırakın. Allah yargılayıcıdır, bağışlayıcıdır. (Tevbe/5)
+        </blockquote>
+        
+        <p>Üstteki ayetlerden kimsenin haberi yoktur ama bu ayeti herkes bilir.</p>
+        <blockquote style="font-size: 0.9em">
+        Dinde zorlama yoktur.[...] (Bakara/256)
+        </blockquote>`
+    },
+    {
+        "question": "İslam'da insanlar arasında eşitlik vardır.",
+        "correct": false,
+        "answer": `
+        <p>Alttaki ayetler diyanet mealinde "(bir zamanlar)" ile düzeltilmeye çalışılmıştır ancak orjinalinde ve diğer bir çok mealde böyle bir düzeltme yoktur. Zira kendini "apaçık" olarak niteleyen bir kitabın neden ek açıklamalara, tefsirlere ihtiyacı vardır? Ayrıca birbirinin tamamen aynısı iki ayetin indirilmesinin anlamı nedir? Allah bu ayeti indirdiğini unutmuş mudur?</p>
+
+        <blockquote style="font-size: 0.9em">
+        Ey İsrailoğulları! Size verdiğim nimetimi ve (bir zamanlar) sizi cümle âleme üstün kıldığımı hatırlayın. (Bakara/47)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Ey İsrailoğulları! Size verdiğim nimetimi ve (bir zamanlar) sizi cümle âleme üstün tuttuğumu hatırlayın. (Bakara/122)
+        </blockquote>
+
+        <p>Aynı zamanda İsrailoğulları (Yahudiler) ve Hristiyanlar yer yer lanetlenmiş, cehennemin en dibinde yakılmışken üstteki ayetler buna büyük bir tezattır.</p>
+        
+        </blockquote>
+        Baksana, biz insanların kimini kiminden nasıl üstün kılmışızdır! Elbette ki ahiret, derece ve üstünlük farkları bakımından daha büyüktür. (İsra/21)
+        </blockquote>`
+    },
+    {
+        "question": "İslam'da cinsiyet eşitliği vardır.",
+        "correct": false,
+        "answer": `<p>Alttaki ayet parantez kullanılarak bir nebze düzeltilmeye çalışılmışsa da Kur'an kadının dövülmesine izin vermektedir.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Allah’ın insanlardan bir kısmını diğerlerine üstün kılmasına bağlı olarak ve mallarından harcama yapmaları sebebiyle <b>erkekler kadınların yöneticisi ve koruyucusudurlar.</b> Sâliha kadınlar Allah’a itaatkârdırlar. Allah’ın korumasına uygun olarak, kimsenin görmediği durumlarda da kendilerini korurlar. (Evlilik hukukuna) <b> baş kaldırmasından endişe ettiğiniz kadınlara öğüt verin, onları yataklarda yalnız bırakın ve onları dövün</b>. Eğer size itaat ederlerse artık onların aleyhine başka bir yol aramayın; çünkü Allah yücedir, büyüktür. (Nisa/34)
+        </blockquote>
+
+        </blockquote>
+        [...]Erkeklerinizden iki şahid tutun; eğer iki erkek bulunmazsa, şahidlerden razı olacağınız bir erkek, biri unuttuğunda diğeri ona hatırlatacak iki kadın olabilir. [...] (Bakara/282)
+        <blockquote style="font-size: 0.9em">
+        
+        <p>Kadınlar yalancı mıdır? Unutkan mıdır? Bu ayetin inme sebebi nedir? <br> Alttaki hadis de hem kadını hem Yahudileri adeta aşağılar.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Resulullah as buyurdular ki: “Biriniz sütresiz olarak namaz kılarsa (önün­den geçtiği takdirde) şunlar namazı bozar: eşek, domuz, yahudi, mecûsi, kadın. Namazın bozulmaması için onun önünden, bunların bir taş atımlık uzaktan geçmesi kifâyet eder.” <br> (Buhari, Salat 90, İlm 18, Ezan 161, Cezau's-Sayd 25; Müslim, Salat 254, (504); Muvatta, Kasru's-Sala)
+        </blockquote>`
+    },
+    {
+        "question": "Muhammed kendi geliniyle evlenmiştir.",
+        "correct": true,
+        "answer": `
+        <p>Günlerden birgün Allah resulü, evlatlığı Zeyd'in evine gidiyor ancak Zeyd o sırada evde değil. 
+        Zeyd'in eşi (aynı zamanda Muhammed'in halasının kızı olan) Zeynep'i uygunsuz bir kıyafetle görüyor. Tabii aşık oluyor Peygamberimiz evlatlığının eşine.Gel zaman git zaman, yüreğindeki sevdayı bastıramayıp gidip Zeynep'i eşi Zeyd'den istiyor. Zeyd ise çok zorlanarak da olsa eşini boşuyor ve Muhammed'le evlendiriyor (Kaynak: Kütüb-i Sitte).<br>Bu kararında alttaki ayet de oldukça etkili oluyor.</p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Bir mümin erkek veya bir mümin kadının, Allah ve resulü bir emir ve hüküm verdiklerinde artık işlerinde bundan başkasını seçme hakları olamaz. Allah’ın ve resulünün emrine itaat etmeyenler doğru yoldan açıkça sapmışlardır. (Ahzab/36)
+        </blockquote>
+
+        <p>
+        Çıkan dedikodular karşısında Muhammed'in evlatlıklarınızın eşleriyle evlenebileceğinizi göstermek için Zeyd'in eşi Zeynep ile evlendirildiğini 
+        söyleyen aşağıdaki ayet iniyor. 
+        </p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Hani Allah'ın nimet verdiği, senin de kendisine iyilik ettiğin kimseye: Eşini yanında tut, Allah'tan kork! diyordun. 
+        Allah'ın açığa vuracağı şeyi, insanlardan çekinerek içinde gizliyordun. Oysa asıl korkmana layık olan Allah'tır. 
+        Zeyd, o kadından ilişiğini kesince biz onu sana nikahladık ki evlatlıkları, karılarıyla ilişkilerini kestiklerinde 
+        (o kadınlarla evlenmek isterlerse) müminlere bir güçlük olmasın. Allah'ın emri yerine getirilmiştir. (Ahzab/37)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an'da zina yasaklanmıştır.",
+        "correct": true,
+        "answer": `<p>Kur'an'da zina İsra/32 ile yasaklanmış, Nur/2 ile cezası acımadan 100 sopa vurmak olarak belirlenmiştir. 
+        Peki aslında zina ne derece engellenmiştir?</p>
+
+        <p>1. Zinanın ispatı için dört tane şahit olması gerekmektedir (Nisa/15). Zira Muhammed'in eşi Ayşe'de zina ile suçlanmış ama 
+        bu ayet dolayısıyla affedilmiştir. Peki sizce zina yapan birinin dört adet şahitle yakalanma ihtimali nedir?</p>
+
+        <p>2. Dört adet eş (Nisa/3), sınırsız cariye (Nisa/3, Nisa/24) ve ücreti ödenme şartıyla geçici nikâh izni (Nisa/24) varken 
+        zinadan uzak durun demenin mantığı nedir?</p>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Sakın zinaya yaklaşmayın; doğrusu bu çirkindir, kötü bir yoldur. (İsra/32)
+        </blockquote>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Zina eden kadın ve erkeğin her birine yüzer değnek vurun. Allah'a ve ahiret gününe inanıyorsanız, Allah'ın dini konusunda o ikisine acımayın. 
+        Onların ceza görmesine, inananlardan bir topluluk da şahit olsun. (Nur/2)
+        </blockquote>
+
+        <blockquote style=\"font-size: 0.9em\">
+        Eğer, (velisi olduğunuz) yetim kızlar (ile evlenip onlar) hakkında adaletsizlik etmekten korkarsanız, (onları değil), size helâl olan (başka) 
+        kadınlardan ikişer, üçer, dörder olmak üzere nikâhlayın.[105] Eğer (o kadınlar arasında da) adaletli davranmayacağınızdan korkarsanız, 
+        o taktirde bir tane alın veya sahip olduğunuz (cariyeler) ile yetinin. Bu, adaletten ayrılmamanız için daha uygundur. (Nisa/3)
+        </blockquote>
+        
+        <blockquote style=\"font-size: 0.9em\">
+        Evli kadınlarla evlenmeniz de haram kılındı. Maliki bulunduğunuz cariyeler müstesna, bunlar, Allah'ın üzerinize farz kıldığı hükümlerdir. 
+        <b>Bunlardan başkasını, zinadan kaçınıp, iffetli olarak, mallarınızla istemeniz size helal kılındı. Onlardan faydalandığınıza mukabil, 
+        kararlaştırılmış olan mehirlerini verin</b>; kararlaştırılandan başka, karşılıklı hoşnud olduğunuz hususda size bir sorumluluk yoktur. 
+        Allah Bilen'dir, Hakim'dir. (Nisa/24) mehir: ücret
+        </blockquote>
+        `
+    },
+    {
+        "question": "Muhammed ayı ikiye yarmıştır.",
+        "correct": true,
+        "answer": `
+        <p>O dönemdeki başka hiçbir kaynakta yer almayan, hiçbir dayanağı olmayan bu olay (şakk-ı kamer) Kur'an'da yer almaktadır.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Kıyamet yaklaştı ve ay yarıldı. (Kamer/1)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an'da evrenin genişlediği yazmaktadır.",
+        "correct": true,
+        "answer": `
+        <p>İslamcılar aşağıdaki ilk ayet ile, modern astronomiyle keşfedilen evrenin genişlediği bilgisinin Kur'an'da mevcut olduğunu iddia eder 
+        ve bunu bir mucize sayar.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Göğü, gücümüzle Biz kurduk; şüphesiz biz onu genişleticiyiz. (Zariyat/47 Eski Diyanet Meali)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Göğü kudretimizle biz kurduk ve şüphesiz bizim (her şeye) gücümüz yeter. (Zariyat/47 Yeni Diyanet Meali)
+        </blockquote>
+
+        <blockquote style="font-size: 0.9em">
+        Yeri de biz döşedik. Biz ne güzel döşeyiciyiz.! (Zariyat/48)
+        </blockquote>
+
+        <p>Peki ilk ayet bir mucize ise, ikinci ayette de yeryüzünün genişlediğini söylemiyor mu? Bu argüman karşısında ise diyanet ayetin mealini 
+        değiştirme yoluna gitmiştir.</p>`
+    },
+    {
+        "question": "Kur'an'da Allah yanılmıştır.",
+        "correct": true,
+        "answer": `
+        <p>Aşağıdaki ayetlerin ilki Uhud savaşından önce inmiş, önceki kitaplarda olduğu gibi savaşta Allah'ın inanlara yardım edeceğini bildirmiştir. 
+        Bu yardımla inananların 1'e 100 oranında üstün olduğu belirtilir.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Ey Peygamber! Mü’minleri savaşa teşvik et. Eğer içinizde sabırlı yirmi kişi bulunursa, iki yüz kişiye galip gelirler. Eğer içinizde (sabırlı) 
+        yüz kişi bulunursa, inkâr edenlerden bin kişiye galip gelirler. Çünkü onlar anlamayan bir kavimdir. (Enfal/65)
+        </blockquote>
+
+        <p>Savaş yapılır ve kaybedilir. Ardından aşağıdaki ayet iner, yeni oran 1'e 2'dir. Peki Allah bu yenilgiyi önceden bilmiyor muydu da bu ayetler indi?</p>
+
+        <blockquote style="font-size: 0.9em">
+        Şimdi ise, Allah yükünüzü hafifletti ve sizde muhakkak bir zaaf olduğunu bildi. Eğer içinizde sabırlı yüz kişi olursa iki yüz kişiye galip gelirler. 
+        Eğer içinizde (sabırlı) bin kişi olursa, Allah’ın izniyle iki bin kişiye galip gelirler. Allah, sabredenlerle beraberdir. (Enfal/66)
+        </blockquote>`
+
+    },
+    {
+        "question": "Ejderhalar gerçektir.",
+        "correct": false,
+        "answer": `<p></p>
+
+        <blockquote style="font-size: 0.9em">
+        Bunun üzerine Mûsâ, asasını yere attı. Bir de ne görsünler, apaçık bir ejderha. (Araf/107)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an köleliği yasaklar.",
+        "correct": false,
+        "answer": `<p></p>
+
+        <blockquote style="font-size: 0.9em">
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an spermin testislerde oluştuğunu bilmiştir.",
+        "correct": false,
+        "answer": `<p>Aksine Kur'an, bilime tamamen ters ve yanlış bir şekilde aşağıdaki ayeti barındırmaktadır.</p>
+
+        <blockquote style="font-size: 0.9em">
+        O (insan), atılan bir sudan yaratıldı. O su, bel (omurga) ve göğüs kafesi arasından çıkar. (Tarık/6-7)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an düşünme organı olan beyinden bahseder.",
+        "correct": false,
+        "answer": `<p>Kur'an, beyinden hiç bahsetmez; düşünme, inanma, sorgulama, şüphelenme yetilerinin hepsinin kalp ile yapıldığını söyler. 
+        Böyle bir şey olmadığını günümüzde pek iyi biliyoruz.</p>
+
+        <blockquote style="font-size: 0.9em">
+        O (insan), atılan bir sudan yaratıldı. O su, bel (omurga) ve göğüs kafesi arasından çıkar. (Tarık/6-7)
+        </blockquote>`
+    },
+    {
+        "question": "Cuma namazı erkeklere farzdır.",
+        "correct": false,
+        "answer": `<p>Çoğu kesim tarafından öyle kabul edilse de Kur'an'da cuma namazıyla ilgili yek ifade alttakidir.</p>
+
+        <blockquote style="font-size: 0.9em">
+        Ey iman edenler! Cuma günü namaz için çağrı yapıldığı zaman, hemen Allah’ın zikrine koşun ve alışverişi bırakın. 
+        Eğer bilirseniz bu, sizin için daha hayırlıdır. (Cuma/9)
+        </blockquote>`
+    },
+    {
+        "question": "Kur'an evrenseldir.",
+        "correct": false,
+        "answer": `<p>Alttaki ayetler çoğu İslamcı tarafından Mekke ve çevresi tüm dünyayı kastetmiştir olarak açıklanmaktadır. 
+        Peki Allah bunun yanlış anlaşılabileceğini düşünemedi de mi bu ayeti indirdi?</p>
+
+        <blockquote style="font-size: 0.9em">
+        Bu indirdiğimiz, kendinden öncekileri doğrulayan, Mekkelileri ve etrafındakileri uyaran mübarek Kitap'dır. Ahirete inananlar buna inanırlar, 
+        namazlarına da devam ederler. (Enam/92) Eski Diyanet Meali
+        </blockquote>
+        <blockquote style="font-size: 0.9em">
+        İşte bu (Kur’an) da, bereket kaynağı, kendinden öncekileri (ilâhî kitapları) tasdik eden ve şehirler anasını (Mekke’yi) ve bütün çevresini 
+        (tüm insanlığı) uyarasın diye indirdiğimiz bir kitaptır. Ahirete iman edenler, ona da inanırlar. Onlar namazlarını vaktinde kılarlar. (Enam/92) Yeni Diyanet Meali
+        </blockquote>
+        
+        <p>Bu ayetde Kur'an'ın anlaşılabilmesi için Arapça indiğini söyler. Peki Arapça indiğinde anlaşılabileceği kişiler Araplar ile sınırlı değil midir?</p>
+
+        <blockquote style="font-size: 0.9em">
+        Biz onu, akıl erdiresiniz diye Arapça bir Kur’an olarak indirdik. (Yusuf/2)
+        </blockquote>
+        
+        `
+    }
+]
